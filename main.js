3e62ce3045f9ef90383be4338da6829c1c7ddf06
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2023-12-07 11:30:04 -0500

    Add function to get computer's choice

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ let computerScore = 0;
 function getRandomNumber() {
     return Math.floor((Math.random() * 3) + 1);
 }
+
+function getComputerChoice() {
+    switch (getRandomNumber()) {
+        case 1:
+            return rock;
+            break;
+        case 2:
+            return paper;
+            break;
+        case 3:
+            return scissors;
+            break;
+    }
+}
