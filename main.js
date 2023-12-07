6959027047ade0d8fef5434cb72c11dc4fa9ae84
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2023-12-07 11:31:44 -0500

    Add round and game functions

--- main.js
+++ main.js
@@ -44,3 +44,65 @@ function getPlayerChoice() {
             playRound();
     }
 }
+
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if (playerChoice == rock) {
+        switch (computerChoice) {
+            case rock:
+                console.log("It's a tie, lets play again!");
+                playRound();
+                break;
+            case paper:
+                console.log("You lose! I chose paper, and paper beats rock.");
+                computerScore++;
+                break;
+            case scissors:
+                console.log("You win! I chose scissors, and rock beats scissors!");
+                playerScore++
+                break;
+        }
+    } else if (playerChoice == paper) {
+        switch (computerChoice) {
+            case rock:
+                console.log("You win! I chose rock, and paper beats rock.");
+                playerScore++;
+                break;
+            case paper:
+                console.log("It's a tie, lets play again!");
+                playRound();
+                break;
+            case scissors:
+                console.log("You lose! I chose scissors, and scissors beats paper.");
+                computerScore++;
+                break;
+        }
+    } else if (playerChoice == scissors) {
+        switch (computerChoice) {
+            case rock:
+                console.log("You lose! I chose rock, and rock beats scissors.");
+                computerScore++;
+                break;
+            case paper:
+                console.log("You win! I chose paper, and scissors beats paper.");
+                playerScore++;
+            case scissors:
+                console.log("It's a tie, lets play again!");
+                playRound()
+                break;
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (playerScore > computerScore) {
+        console.log(`You win! You: ${playerScore} Computer: ${computerScore}`);
+    }
+    if (computerScore > playerScore) {
+        console.log(`You lose! You: ${playerScore} Computer: ${computerScore}`);
+    }
+}
